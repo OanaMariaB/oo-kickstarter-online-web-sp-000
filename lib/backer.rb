6103116b0ad5b840cb3f8846e4904770978bc48d
Oana Berentan <oana.berentan@yahoo.com>
2019-06-17 21:15:08 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-  backer.add_project(self) unless backer.backed_projects.include(self)
+  backer.back_project(self) unless backer.backed_projects.include(self)
   end
 
 end
